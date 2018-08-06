030a4d22f583f3e806a89bde2df1541a2ba845a2
nao-kobayashi <nao.kobayashi@nifty.com>
2018-08-06 19:08:03 +0900

    refactor: refactor all code

--- src/main.rs
+++ src/main.rs
@@ -3,175 +3,103 @@ extern crate encoding;
 
 use std::env::args;
 use std::fs::File;
+use std::error::Error;
 use memmap::MmapOptions;
 use encoding::{ Encoding, DecoderTrap };
 use encoding::all::WINDOWS_31J;
 
-//初回に開いた際に出力開始インデックスを取得する。
-fn get_read_start_pos(mmap: &memmap::Mmap, length: usize, disp_rows: i32) -> usize {
+fn print_vec(buffer: Vec<u8>) {
+    let lines: _ = buffer.split(|buf| *buf == b'\n')
+        .filter(|buf| buf.len() > 0)
+        .map(|buf| { encode(buf).unwrap_or("".to_owned()) });
+
+    for line in lines {
+        println!("{}", line);
+    }
+}
+
+fn find_start_pos(mmap: &memmap::Mmap, length: usize, disp_rows: i32) -> usize {
+    //ファイルサイズはsliceインデックスで見るから-1
     let mut index: usize = length - 1;
     let mut counter = disp_rows;
-    loop {
+    while counter > 0 {
         if index <= 1 { return 0; }
         if &mmap[index..(index + 1)] == b"\n" {
             counter -= 1;
-            //最後に見つかった改行分は出力不要
-            if counter < 0 { 
-                break; 
-            } else {
-                index -= 1;
-            }
-        } else {
-            index -= 1;
         }
+        index -= 1;
     }
-    index
+    //+1しないと先頭の改行が出てしまう。
+    index + 1
 }
 
-//バッファしたデータを出力する。
-fn print_vec(buffer: Vec<u8>) {
-    let lines: _ = buffer.split(|b| *b == b'\n');
-    for line in lines {
-        if line.len() == 0 { continue; }
-        let cnv_result = if let Some(cnv_result) = encode(line) {
-            cnv_result
-        } else  {
-            println!("parse error.");
-            std::process::exit(-1);
-        };
-       
-        println!("{}", cnv_result);
-    }
-}
-
-//ファイルを開いてmemmap生成
-fn get_memmap(file_path: &str) -> memmap::Mmap {
-    let file = match File::open(file_path) {
-        Ok(file) => file,
-        Err(e) => {
-            println!("file open error {:?}", e);
-            std::process::exit(-1);
-        }
-    };
-
+fn read_file(file_path: &str, start_pos: Option<usize>, disp_rows: i32) -> Result<Option<(Vec<u8>, usize)>, Box<Error>> {
     let mmap = unsafe { 
-        match MmapOptions::new().map(&file) {
-            Ok(map) => map,
-            Err(e) => {
-                println!("memmap error {:?}", e);
-                std::process::exit(-1);
-            }
-        }
+        let file = File::open(file_path)?;
+        MmapOptions::new().map(&file)? 
     };
 
-    mmap
-}
-
-//初回のファイルを読み込む関数
-fn read_file(file_path: &str, disp_rows: i32) -> ReadResult {
-    let mmap = get_memmap(file_path);
     let length = *(&mmap.len() as &usize);
-    let start_pos = get_read_start_pos(&mmap, length, disp_rows);
+    let limit = if let Some(limit) = start_pos {
+        limit
+    } else {
+        find_start_pos(&mmap, length, disp_rows)
+    };
 
-    ReadResult {
-        read_buffer: mmap[start_pos..length].to_vec(),
-        buf_length: length,
+    if length <= limit { 
+        Ok(None)
+    } else {
+        Ok(Some((mmap[limit..length].to_vec(), length)))
     }
-}
-
-//2回目以降のファイル読み込み関数 前回読んだ位置からファイルの終端まで
-fn read_file_remain_all(file_path: &str, start_pos: usize) -> Option<ReadResult> {
-    let mmap = get_memmap(file_path);
-
-    let length = *(&mmap.len() as &usize);
-    if length <= start_pos {
-        return None;
-    }
-
-    Some(ReadResult {
-        read_buffer: mmap[start_pos..length].to_vec(),
-        buf_length: length,
-    })
-}
-
-//ファイル読み込み結果を格納する構造体
-struct ReadResult {
-    read_buffer: Vec<u8>,
-    buf_length: usize,
 }
 
 fn encode(buffer: &[u8]) -> Option<String> {
     if let Some(utf) = encode_utf8(buffer) {
-        return Some(utf);
+        Some(utf)
     } else {
-        if let Some(sjis) = encode_shit_jis(buffer) {
-            return Some(sjis);
-        } else {
-            return None;
-        }
+        encode_shit_jis(buffer)
     }
 }
     
 fn encode_shit_jis(buffer: &[u8]) -> Option<String> {
     let mut chars = String::new();
-    match WINDOWS_31J.decode_to(&buffer.to_vec(), DecoderTrap::Replace, &mut chars) {
-        Ok(_) => Some(chars),
-        Err(e) => {
-            println!("Shift Jis parse error.{:?}", e);
-            None
-        },
+    if let Ok(_) = WINDOWS_31J.decode_to(&buffer.to_vec(), DecoderTrap::Replace, &mut chars) {
+        return Some(chars);
     }
+    None
 }
 
 fn encode_utf8(buffer: &[u8]) -> Option<String> {
     if let Ok(output) = String::from_utf8(buffer.to_vec()) {
-        Some(output)
-    }  else {
-        None
+        return Some(output)
     }
+    None
 }
 
-
 fn main() {
-    let args: Vec<String> = args().collect();
-
+    let mut args = args().skip(1);
     //第1引数はファイル名（必須）
-    let file_path = &args[1];
-
+    let file_path = args.next().expect("ファイル名が指定されていません");
     //第2引数は初回の表示行数 デフォルト20行
-    let disp_rows: i32 = if args.len() < 3 {
-        20
-    } else {
-        match args[2].parse(){
-            Ok(row) => row,
-            Err(_) => 20
-        }
-    };
-
+    let disp_rows: i32 = if let Some(p) = args.next() { p.parse::<i32>().unwrap_or(20) } else { 20 };
     //第3引数はファイルの監視時間間隔 デフォルト3秒
-    let refresh_sec: u64 = if args.len() < 4 {
-        3
-    } else {
-        match args[3].parse(){
-            Ok(sec) => sec,
-            Err(_) => 3
-        }
-    };
+    let refresh_sec: u64 = if let Some(p) = args.next() { p.parse::<u64>().unwrap_or(3) } else { 3 };
 
-    //初回のファイル読み込みと出力
-    let read_result = read_file(file_path, disp_rows);
-    let mut length = read_result.buf_length;
-    print_vec(read_result.read_buffer);
-
+    let mut position = None;
     loop {
         //ファイルが書き換わったら表示する。
-        match read_file_remain_all(file_path, length) {
-            Some(x) => {
-                print_vec(x.read_buffer);
-                length = x.buf_length;
+        match read_file(&file_path, position, disp_rows) {
+            Ok(x) => {
+                if let Some(x) = x  {
+                    print_vec(x.0);
+                    position = Some(x.1);
+                } else {
+                    std::thread::sleep(std::time::Duration::from_secs(refresh_sec));
+                }
             },
-            None => {
-                std::thread::sleep(std::time::Duration::from_secs(refresh_sec));
+            Err(e) => {
+                println!("read_file error {:?}", e);
+                std::process::exit(-1);
             }
         };
     }
