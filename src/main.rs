b55e886e9ad301f90976f5dc46a28646f2e3cd01
nao-kobayashi <nao.kobayashi@nifty.com>
2018-08-10 17:30:44 +0900

    refactor: read_file関数をクロージャへ

--- src/main.rs
+++ src/main.rs
@@ -25,29 +25,12 @@ fn find_start_pos(mmap: &memmap::Mmap, length: usize, mut counter: i32) -> usize
     0
 }
 
-fn read_file(file_path: &str, start_pos: Option<usize>, disp_rows: i32) -> Result<Option<(Vec<u8>, usize)>, Box<Error>> {
-    let file = File::open(file_path)?;
-    let mmap = unsafe { MmapOptions::new().map(&file)? };
-    let length = *(&mmap.len() as &usize);
-
-    let limit = if let Some(limit) = start_pos {
-        limit
-    } else {
-        find_start_pos(&mmap, length, disp_rows)
-    };
-
-    if length <= limit {
-        return Ok(None);
-    }
-    Ok(Some((mmap[limit..length].to_vec(), length)))
-}
-
 fn decode(buffer: &[u8]) -> Option<String> {
     let mut output = String::new();
 
     if let Ok(output) = String::from_utf8(buffer.to_vec()) {
         return Some(output)
-    } else if let Ok(_) = WINDOWS_31J.decode_to(&buffer.to_vec(), DecoderTrap::Replace, &mut output) {
+    } else if let Ok(_) = WINDOWS_31J.decode_to(buffer, DecoderTrap::Replace, &mut output) {
         return Some(output);
     }
 
@@ -57,20 +40,36 @@ fn decode(buffer: &[u8]) -> Option<String> {
 fn main() {
     let mut args = std::env::args().skip(1);
     let file_path = args.next().expect("ファイル名が指定されていません");    //第1引数はファイル名（必須）
-    let disp_rows: i32 = if let Some(p) = args.next() { p.parse::<i32>().unwrap_or(20) } else { 20 };   //第2引数は初回の表示行数 デフォルト20行
+    let display_rows: i32 = if let Some(p) = args.next() { p.parse::<i32>().unwrap_or(20) } else { 20 };   //第2引数は初回の表示行数 デフォルト20行
     let refresh_sec: u64 = if let Some(p) = args.next() { p.parse::<u64>().unwrap_or(3) } else { 3 };   //第3引数はファイルの監視時間間隔 デフォルト3秒
 
     let mut position = None;
+    let mut f_read_file = |file_path: &str| -> Result<Option<(Vec<u8>)>, Box<Error>> {
+        let file = File::open(file_path)?;
+        let mmap = unsafe { MmapOptions::new().map(&file)? };
+        let length = *(&mmap.len() as &usize);
+
+        let start_pos = if let Some(start_pos) = position {
+            if length <= start_pos {
+                return Ok(None);
+            }
+            start_pos
+        } else {
+            find_start_pos(&mmap, length, display_rows)
+        };
+
+        position = Some(length);
+        Ok(Some(mmap[start_pos..length].to_vec()))
+    };
+
     loop {
-        let result = read_file(&file_path, position, disp_rows).expect("ファイルの読み込みに失敗しました");
+        let result = f_read_file(&file_path).expect("ファイルの読み込みに失敗しました");
         if let Some(result) = result {
-            if let Err(e) = print_vec(result.0) {
+            if let Err(e) = print_vec(result) {
                 panic!(format!("write error:{}", e));
             }
-            position = Some(result.1);
         } else {
             std::thread::sleep(std::time::Duration::from_secs(refresh_sec));
         }
     }
 }
-
